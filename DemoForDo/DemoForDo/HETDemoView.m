d94ed4d068c91efbd7d1f49625fe575bb58a01c9
Start <250226337@qq.com>
2018-05-08 14:00:38 +0800

    消除导航栏的横线

--- DemoForDo/DemoForDo/HETDemoView.m
+++ DemoForDo/DemoForDo/HETDemoView.m
@@ -9,14 +9,14 @@
 //
 
 #import "HETDemoView.h"
-#define margin 8
+#define margin 4
 @implementation HETDemoView
 
 
 - (void)drawRect:(CGRect)rect {
     [[UIColor grayColor]set];//设置颜色 填充颜色
     //1.画一个圆角矩形以及一个头尖角
-    UIBezierPath *berierPath = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(margin, margin, self.bounds.size.width-2*margin, self.bounds.size.height-2*margin) cornerRadius:10.0];
+    UIBezierPath *berierPath = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(margin, margin, self.bounds.size.width-2*margin, self.bounds.size.height-2*margin) cornerRadius:11];
     berierPath.lineWidth = 1.0;
     berierPath.lineCapStyle = kCGLineCapRound; //线条拐角
     berierPath.lineJoinStyle = kCGLineJoinRound; //终点处理、
@@ -27,8 +27,12 @@
     [berierPath fill];//贝塞尔线进行填充
     [berierPath stroke];//贝塞尔线进行画笔填充
     NSString *string = @"送金蛋";
-    NSDictionary *dict  =@{NSFontAttributeName:[UIFont systemFontOfSize:12],NSForegroundColorAttributeName:[UIColor whiteColor]};
-    [string drawInRect:CGRectMake((self.bounds.size.width-4*margin)/2,(self.bounds.size.height-margin*2)/2, self.bounds.size.width, self.bounds.size.height) withAttributes:dict];
+//    NSDictionary *dict  =@{NSFontAttributeName:[UIFont systemFontOfSize:6],NSForegroundColorAttributeName:[UIColor whiteColor]};
+//    [string drawInRect:CGRectMake((self.bounds.size.width-4*margin)/2,(self.bounds.size.height-margin*2)/2, self.bounds.size.width, self.bounds.size.height) withAttributes:dict];
+    
+    
+    NSDictionary *dict  =@{NSFontAttributeName:[UIFont systemFontOfSize:(10.0)],NSForegroundColorAttributeName:[UIColor whiteColor]};
+    [string drawInRect:CGRectMake((self.bounds.size.width-(30))*0.5,margin, self.bounds.size.width, self.bounds.size.height) withAttributes:dict];
     
 
 }
--- DemoForDo/DemoForDo/ViewController.m
+++ DemoForDo/DemoForDo/ViewController.m
@@ -13,12 +13,16 @@
 @interface ViewController ()
 /**label*/
 @property(nonatomic,strong)HETMyInteralHeadLabelView *label;
+@property (nonatomic, strong) UIImageView *shadowImage;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+  
+    [self.navigationController.navigationBar setBarTintColor:[UIColor redColor]];
     // Do any additional setup after loading the view, typically from a nib.
     self.label = [[HETMyInteralHeadLabelView alloc]initWithtCornerRadius:16 borderWidth:1 borderColor:[UIColor blackColor] fontSize:8.0];
     self.label.frame = CGRectMake(50, 50, 32, 32);
@@ -31,11 +35,37 @@
     sendView.sendViewColor = [UIColor orangeColor];
     sendView.sendViewStr = @"送金蛋";
     
-    HETDemoView *demoView = [[HETDemoView alloc]initWithFrame:CGRectMake(100, 300, 100, 44)];
+    HETDemoView *demoView = [[HETDemoView alloc]initWithFrame:CGRectMake(100, 300, 44, 22)];
     [self.view addSubview:demoView];
     demoView.backgroundColor = [UIColor clearColor];
 }
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    NSArray *subViews = allSubviews(self.navigationController.navigationBar);
+    for (UIView *view in subViews) {
+        if ([view isKindOfClass:[UIImageView class]] && view.bounds.size.height<1){
+            //实践后发现系统的横线高度为0.333
+            self.shadowImage =  (UIImageView *)view;
+        }
+    }
+    self.shadowImage.hidden = YES;
 
+}
+NSArray *allSubviews(UIView *aView) {
+    NSArray *results = [aView subviews];
+    for (UIView *eachView in aView.subviews)
+    {
+        NSArray *subviews = allSubviews(eachView);
+        if (subviews)
+            results = [results arrayByAddingObjectsFromArray:subviews];
+    }
+    return results;
+}
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    self.shadowImage.hidden = NO;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
